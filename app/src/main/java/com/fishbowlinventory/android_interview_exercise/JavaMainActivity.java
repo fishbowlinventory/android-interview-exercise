ca3339c5b3ce6433fda5d2d5cacd6e0ce68d5f91
Davis Pearson <daviscodesbugs@gmail.com>
2022-03-30 07:35:42 -0600

    Update dependencies and libraries

--- app/src/main/java/com/fishbowlinventory/android_interview_exercise/JavaMainActivity.java
+++ app/src/main/java/com/fishbowlinventory/android_interview_exercise/JavaMainActivity.java
@@ -1,8 +1,8 @@
 package com.fishbowlinventory.android_interview_exercise;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
@@ -22,20 +22,20 @@ public class JavaMainActivity extends AppCompatActivity {
         items.add(new Item(10.00F, "Bird"));
         items.add(new Item(21.99F, "Pig"));
 
-        items = removeLessThan(items, Float.valueOf(10));
+        items = removeLessThan(items, 10F);
         // BonusKt.removeLessThan(items, Float.valueOf(10)); // TODO: Bonus (See Bonus.kt)
     }
 
     // 1. Traverse array (one time) removing all items with price <= x
     private ArrayList<Item> removeLessThan(ArrayList<Item> itemList, Float price) {
         // TODO: Implement here
-        return new ArrayList();
+        return new ArrayList<>();
     }
 
     // 2. Generate the sum of item prices
     private Float sumItemPrices(ArrayList<Item> itemList) {
         // TODO: Implement here
-        return Float.valueOf(0);
+        return 0F;
     }
 
     // 3. Display the sum centered vertically and horizontally in the main view
